b16e890bf9f6cbd53bdf0d5b654c40883d418797
macdh <macdhollister@gmail.com>
2019-06-06 02:31:21 -0700

    Made snake's head red, updated snake box size

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
-const sn = new Snake();
-
 // Environment Variables
 const BOARD_WIDTH = 14;
 const BOARD_HEIGHT = 14;
 const GRID_SIZE = 50;
+
+const sn = new Snake();
 
 // Create canvas and context variables
 const canvas = document.getElementById('myCanvas');
@@ -32,12 +32,15 @@ setInterval(() => {
     ctx.fillStyle = '#000000';
     ctx.fillRect(0,0,canvas.width,canvas.height);
 
-    sn.move();
+    sn.move(BOARD_WIDTH, BOARD_HEIGHT);
 
     // repaint canvas with new snake position
-    ctx.fillStyle = '#ffffff';
     let positions = sn.getPos();
-    for (let i = 0; i < positions.length; i++) {
-        ctx.fillRect(positions[i].x * GRID_SIZE, positions[i].y * GRID_SIZE, 50, 50)
+    ctx.fillStyle = '#ff0000' // red
+    ctx.fillRect(positions[0].x * GRID_SIZE + 5, positions[0].y * GRID_SIZE + 5, 4*GRID_SIZE/5, 4*GRID_SIZE/5)
+
+    ctx.fillStyle = '#ffffff';
+    for (let i = 1; i < positions.length; i++) {
+        ctx.fillRect(positions[i].x * GRID_SIZE + GRID_SIZE/20, positions[i].y * GRID_SIZE + GRID_SIZE/20, 9*GRID_SIZE/10, 9*GRID_SIZE/10)
     }
 }, 100);
