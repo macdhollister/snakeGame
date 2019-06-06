fe46417ae46f5c62f959fb6e25164043b3d8b520
macdh <macdhollister@gmail.com>
2019-06-06 00:50:11 -0700

    added getters/setters to track and change position

--- Snake.js
+++ Snake.js
@@ -7,29 +7,78 @@
  */
 class Snake {
     constructor() {
-        this.x = 0;
-        this.y = 0;
-        this.xspeed = 1;
-        this.yspeed = 0;
+        this.position = {
+            x: 0,
+            y: 0
+        }
+        this.speed = {
+            xspeed: 1,
+            yspeed: 0
+        }
+
+        this.newSpeed = {
+            xspeed: 1,
+            yspeed: 0
+        }
     }
 
     // ---------- Getters ----------
 
     // Return snake's position
     getPos() {
-        return {
-            x: this.x,
-            y: this.y
-        }
+        return this.position;
     }
-
-    getDirection() {
-        return {
-            xspeed: this.xspeed,
-            yspeed: this.yspeed
-        }
-    }
-
 
     // ---------- Setters ----------
+
+    move() {
+        this._setNewSpeed();
+        this.position.x += this.speed.xspeed;
+        this.position.y += this.speed.yspeed;
+    }
+
+    // Change snake's direction
+    _setNewSpeed() {
+        // change this.speed according to this.newSpeed
+        // restrict according to current direction
+
+        if (this.speed.xspeed !== -1 * this.newSpeed.xspeed ||
+            this.speed.yspeed !== -1 * this.newSpeed.yspeed) {
+                this.speed = this.newSpeed;
+        }
+    }
+
+    // Set next direction
+    changeDirection(dir) {
+        // set this.newSpeed according to input
+        switch (dir) {
+            case 37:
+                this.newSpeed = {
+                    xspeed: -1,
+                    yspeed: 0
+                }
+                break;
+
+            case 38:
+                this.newSpeed = {
+                    xspeed: 0,
+                    yspeed: -1
+                }
+                break;
+
+            case 39:
+                this.newSpeed = {
+                    xspeed: 1,
+                    yspeed: 0
+                }
+                break;
+
+            case 40:
+                this.newSpeed = {
+                    xspeed: 0,
+                    yspeed: 1
+                }
+                break;
+        }
+    }
 }
