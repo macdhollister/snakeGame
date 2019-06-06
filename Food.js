f3d230192def2b4f1baff6a0bd9e19ed61a7c6c1
macdh <macdhollister@gmail.com>
2019-06-06 15:15:01 -0700

    New food position directly sets position now

--- Food.js
+++ Food.js
@@ -7,17 +7,14 @@ class Food {
         this.boardWidth = boardWidth;
         this.boardHeight = boardHeight;
 
-        // this.position = this.newPosition();
-        this.position = { x: 0, y: 0 }
+        this.newPosition();
         while (snakePositions.filter(pos => pos.x === this.position.x && pos.y === this.position.y).length > 0) {
-            this.position = this.newPosition()
+            this.newPosition()
         }
-
-        console.log(this.position)
     }
 
     newPosition() {
-        return {
+        this.position = {
             x: Math.floor(Math.random() * this.boardWidth),
             y: Math.floor(Math.random() * this.boardHeight)
         }
